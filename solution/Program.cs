c92b6fe9cee699ffea111d3c5e8d5ba8a8160d0a
Rizo <rizo2@MUHAMMADRIZO>
2023-07-11 20:18:39 +0500

    add changes

---
+++ solution/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("hello hello ");
+int a = 15, b = 20;
+Console.WriteLine(a + b);
